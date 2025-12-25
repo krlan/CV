7cfca48ecfc7e389c5dad08a6300e9e8fc86840b
Igor Trefilov <i@trefeelove.com>
2025-12-25 20:01:41 +0700

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Will work with GitHub Pages when using GitHub Actions
+  base: '/CV/', // GitHub repo name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
